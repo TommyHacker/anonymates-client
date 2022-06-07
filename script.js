6fae809202077cc3a135551440324572ffa27a3f
mariesophiecd <mariesophie.cduran@gmail.com>
2022-06-07 10:21:14 +0100

    added changes to script.js

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const form = document.querySelector('form');
+// const url = 'https://anonymates.herokuapp.com/article';
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
+    // dont refresh the page when form submitted.
     e.preventDefault();
+    // get text area / body text from form
     const body = e.target.body.value;
+    // get title from form
     const title = e.target.title.value;
-    const data = { body, title }
+    // put body and title into "data" payload
+    const data = { body, title };
+    // for heroku
+    const url = 'https://anonymates.herokuapp.com/articles';
+    // for localhost
+    const localUrl = 'http://localhost:3000/articles';
 
-    fetch('https://anonymates.herokuapp.com/articles', {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-      })
-      .then(res => res.json())
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+    const result = fetch('https://anonymates.herokuapp.com/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // destructure data into string
+        body: JSON.stringify({ data }),
+        // ignore cors policy
+        cors: 'no-cors',
+    })
+        // parse response
+        .then((res) => res.json())
+        // return data once parsed
+        .then((res) => {window.location.replace(`./single-entry.html?id=${res.data.id}`)})
+        // catch any errors
+        .catch((err) => console.log(err));
+});
 
-    //console.log(e.target.title.value);
-})
-
-
-
-const articlesFetch = () => {
+// this gets an Array of all articles!
+const articlesFetch = async () => {
+    // fetch the array string
     fetch('https://anonymates.herokuapp.com/articles')
-    .then(res => res.json())
-    .then(response => console.log(response))
-}
+        // parse to javascript
+        .then((res) => res.json())
+        // do something with the data
+        .then((data) => console.log(data))
+        // catch any error
+        .catch((err) => console.log(err));
+};
 
+// get all articles on page load
 articlesFetch();
