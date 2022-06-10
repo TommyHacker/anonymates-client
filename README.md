b8587dbae51d0a6af808dea39e590f155511af46
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-10 13:51:50 +0100

    added readme changed giphy.js

--- README.md
+++ README.md
@@ -1,12 +1,62 @@
-# Anonymates App
+# al_jazari_lap_1_assignment_TommyHacker
 
-The main goal of the app is to allow users to post articles anonymously.
-They can also:
+## The main goal of the app is to allow users to post articles anonymously.
+
+## They can also:
+
 - have access to all the articles posted
 - react to those articles with emojis and a like button
-- leave comments 
+- leave comments
 - post GIFS
 
+## installation
 
+- clone repo
+- cd into anonymates-client -<code>npm install</code>
 
+<hr>
 
+## Usage
+
+## client directory:
+
+- <code> start index.html </code>
+
+  <hr>
+
+# Changelog
+
+## index.html
+
+- refactored scripts
+-
+- complete fetch requests to api
+-
+
+<hr>
+
+## bugs
+
+<hr>
+
+## wins and challenges
+
+## wins
+
+-
+
+-
+
+-
+
+-
+
+### challenges
+
+- giphy http response being rejected by netlify https host;
+
+- using FS to store json data and update accordingly;
+
+- styling with bootstrap and remaining responsive;
+
+- finding a way to reject users who try to like an article more than once. Also as similar issue when it came to adding reactions.
--- scripts/giphy.js
+++ scripts/giphy.js
@@ -26,60 +26,64 @@ const get_Gif = async (e) => {
 	})
 		.then((res) => res.json())
 
-		.then((response) => (data = response.data))
+		.then(
+			(response) =>
+				response.data.forEach((e) => {
+					//getting the already empty div
+					const exp007 = document.getElementById('exp007');
+					//creating the img element
+					const imgz = document.createElement('img');
+					//creating the h6 element
+					// const h6 = document.createElement('h6')
+					//drrting the h6's text-content to the title of each result of GIPHY api fetch data
+					// h6.textContent = e.title
+					//bootstrap marigin-top: 3something
+					// h6.classList = "mt-3"
+					//setting the created img's src to the nested giphy url
+					imgz.src = e;
+					//bootstrap class of width 50% i believe
+					imgz.classList = 'p-2';
+					// setting img tag to have a cursor pointer when hovered
+					imgz.style = 'cursor: pointer;height:170px;width:170px;';
+
+					console.log(exp007.childElementCount);
+					// exp007.childElementCount >= 8 ?  false : ''
+					//setting an event of click on img tags so can pick a GIF and fisplay in our comment-reply area
+					imgz.addEventListener('click', (e) => {
+						//clg event target's src
+						console.log(e.target.src);
+						// document.getElementById('textA').textContent = e.target.src;
+						let url = e.target.src;
+						giphyUrl = e.target.src;
+						commentImage.src = imgz.src;
+					});
+
+					// appending our creations from the forEach Loop
+					// exp007.append(h6, imgz)
+					exp007.append(imgz);
+				})
+
+			// data[0].images.fixed_height.url
+		)
 		.catch((err) => console.log(err));
-
-	// turning that fetch response to json
-	// const api_data = await resp.json();
-
-	//destruction of data from the json response
-
-	//console.logs for me to check
-	// console.log(resp);
-	// console.log(api_data);
-	// console.log(data);
-
-	//forEach() looping thru that fetched data and creating, appending and adding styles each loop
-	//setting GIPHY URL to our created img tag src="" to finally dispplay GIF
-
-	const commentImage = document.querySelector('#comment-image');
-
-	data.forEach((e) => {
-		//getting the already empty div
-		const exp007 = document.getElementById('exp007');
-		//creating the img element
-		const imgz = document.createElement('img');
-		//creating the h6 element
-		// const h6 = document.createElement('h6')
-		//drrting the h6's text-content to the title of each result of GIPHY api fetch data
-		// h6.textContent = e.title
-		//bootstrap marigin-top: 3something
-		// h6.classList = "mt-3"
-		//setting the created img's src to the nested giphy url
-		imgz.src = e;
-		//bootstrap class of width 50% i believe
-		imgz.classList = 'p-2';
-		// setting img tag to have a cursor pointer when hovered
-		imgz.style = 'cursor: pointer;height:170px;width:170px;';
-
-		console.log(exp007.childElementCount);
-		// exp007.childElementCount >= 8 ?  false : ''
-		//setting an event of click on img tags so can pick a GIF and fisplay in our comment-reply area
-		imgz.addEventListener('click', (e) => {
-			//clg event target's src
-			console.log(e.target.src);
-			// document.getElementById('textA').textContent = e.target.src;
-			let url = e.target.src;
-			giphyUrl = e.target.src;
-			commentImage.src = imgz.src;
-		});
-
-		// appending our creations from the forEach Loop
-		// exp007.append(h6, imgz)
-		exp007.append(imgz);
-	});
-	// data[0].images.fixed_height.url
 };
+
+// turning that fetch response to json
+// const api_data = await resp.json();
+
+//destruction of data from the json response
+
+//console.logs for me to check
+// console.log(resp);
+// console.log(api_data);
+// console.log(data);
+
+//forEach() looping thru that fetched data and creating, appending and adding styles each loop
+//setting GIPHY URL to our created img tag src="" to finally dispplay GIF
+
+const commentImage = document.querySelector('#comment-image');
+console.log(data);
+data;
 
 //setting event on form of submit and passing get_gif Function as a 2nd parameter
 const gif_formz = document.getElementById('gif-formz');
