5861b45d7f52aff3d511b7282d9092275515463d
“Anka21” <klimekanna@icloud.com>
2022-06-10 09:21:28 +0100

    about-page.html

--- _test_/script.spec.js
+++ _test_/script.spec.js
@@ -1,17 +1,17 @@
-// const fs = require('fs');
-// const path = require('path');
-// const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
 
-// describe('script.js', () => {
-//     // beforeEach(() => {
-//     //     document.documentElement.innerHTML = html.toString();
-//     // })
-//     describe('head', () => {
-//         test('it has a title', () => {
-//             let title = document.querySelector('title');
-//             expect(title).toBeTruthy();
-//         })
-//     })
+describe('script.js', () => {
+    // beforeEach(() => {
+    //     document.documentElement.innerHTML = html.toString();
+    // })
+    describe('head', () => {
+        test('it has a title', () => {
+            let title = document.querySelector('title');
+            expect(title).toBeTruthy();
+        })
+    })
 
-// })
+})
